3f00130b6dba49e332500219cce2d5c20e78141b
Luke Menard <lukemenard@gmail.com>
2019-07-12 21:22:25 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new << yield(source_array[i])
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, starting_value = 0)
+  i = 0
+  new_value = 0
+  
+  
+  if source_array.include?(nil) && source_array.include?(true)
+    return true
+  elseif source_array.include?(nil) && source_array.include?(false)
+    return false
+  elsif source_array.include?(false)
+    return false
+  elsif source_array.any? == false
+    return false
+
+
+    
+  else
+    while i == 0 do
+      new_value = yield(source_array[i], starting_value)
+      i += 1
+    end
+    while i > 0 && i < source_array.length do
+      new_value += yield(source_array[i], starting_value)
+      new_value = new_value - starting_value
+      i += 1
+    end
+  end
+
+  return new_value
+end
